037af4de14872484722092aae434b70c17b44388
Igor Łaskawiec <77580949+Igorlaskawiec@users.noreply.github.com>
2022-10-20 11:11:12 +0200

    update swap function
    added comment of optional version without swap function

--- Bubble Sort/BubbleSort.cpp
+++ Bubble Sort/BubbleSort.cpp
@@ -7,7 +7,14 @@ void BubbleSort(int arr[], int n){
 		for(int j =1; j<n;j++){
 			if(arr[j-1]>arr[j]){
 			
-			swap(arr[j-1],arr[j]);
+			swap(arr[j-1],arr[j]); 
+			/*
+			int tmp = arr[j-1];
+			arr[j-1] = arr[j];
+			arr[j] = tmp;
+			*/
+			//Without swap function
+			
 		}
 		}
 	}

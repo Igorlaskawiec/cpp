0ed1cc4ca8e65ab9e7e2e7740aacbb8752879097
Igor Łaskawiec <77580949+Igorlaskawiec@users.noreply.github.com>
2022-10-20 10:43:24 +0200

    add Bubble Sort code

---
+++ Bubble Sort/BubbleSort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void BubbleSort(int arr[], int n){
+	for(int i = 0; i<n; i++){
+		for(int j =1; j<n;j++){
+			if(arr[j-1]>arr[j]){
+			
+			swap(arr[j-1],arr[j]);
+		}
+		}
+	}
+	
+}
+
+int main(){
+	
+	int a;
+	cout<<"Array length: "<<endl;
+	cin>>a;
+	
+	int arr[a];
+	
+	for (int i=0; i<=a-1; i++) {
+		cout<< "Give number "<<i<<" of "<<a<<": "; cin>>arr[i];
+	}	
+	BubbleSort(arr,a);
+	
+		for(int i=0;i<a;i++)
+          cout<<arr[i]<<" ";
+}
